6714da74dfcb4a21a6a326e3de0f9c4a0fb05b5b
Meg <yusuffrasheed.yr@gmail.com>
2022-04-15 11:53:58 +0100

    laban

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
-app.use((req,res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(cors());
+app.use(cors({
+    origin: "*",
+}))
+// app.use((req,res, next)=>{
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 const cloudinary = require('cloudinary');
 const mongoose = require('mongoose');
 
