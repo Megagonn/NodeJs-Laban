79ab98d50474a3cc1e9f3231aae5e543f3c5746b
Meg <yusuffrasheed.yr@gmail.com>
2022-05-10 08:39:27 +0100

    product model modidfied

--- models/product.model.js
+++ models/product.model.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     name:String,
     price:String,
+    shippingFee:String,
     category:String,
     discount:Boolean,
-    new:Boolean,
-    picture:String,
+    // new:Boolean,
+    // picture:String,
     pictureUrl:String,
     description:String,
     quantity:Number,
