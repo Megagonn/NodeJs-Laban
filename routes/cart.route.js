d3f4e2af94d82ac7e4dabbab0475e869094f0d62
Meg <yusuffrasheed.yr@gmail.com>
2022-08-16 10:43:35 +0100

    commit

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -10,7 +10,7 @@ router.post('/addtocart', (req, res) => {
     // const {email, items} = req.body;
     const form = new formidable.IncomingForm();
     form.parse(req, (error, fields, files) => {
-        if (req.body != null) {
+        if (fields != null) {
             let formData = new cartModel(fields);
             formData.save((err) => {
                 console.log(err);
