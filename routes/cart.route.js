8276cec1761a3d3ac2dde56e8e7a949ed9836dc5
Meg <yusuffrasheed.yr@gmail.com>
2022-08-16 22:53:23 +0100

    cart increment and decrement API created.

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -6,23 +6,76 @@ const formidable = require('formidable');
 const cartModel = require('../models/cart.model')
 
 
-router.post('/addtocart', (req, res) => {
+router.post('/add-to-cart', (req, res) => {
     // const {email, items} = req.body;
-    const form = new formidable.IncomingForm();
+    let form = new formidable.IncomingForm();
     form.parse(req, (error, fields, files) => {
         if (fields.email != null || fields.email != '') {
-            let formData = new cartModel(fields);
-            formData.save((err) => {
-                console.log(err);
-            });
+            cartModel.find({email:fields.email},(err,result)=>{
+                if (result.length>0) {
+                    res.send("Product is already in cart")
+                }else{
+                    let formData = new cartModel(fields);
+                    formData.save((err) => {
+                        console.log(err);
+                    });
+                }
+            })
+            
         } else{
-            console.log('Fill all data');
-            res.send('Fill all data')
+            console.log('Invalid request');
+            res.send('Invalid request')
         }
     })
     // console.log([email, items]);
     // res.send("<p>I got your request...Processing...</p>");
 
+})
+
+router.patch('/increase-item-count',(req,res)=>{
+    let form = new formidable.IncomingForm();
+    form.parse(req, (error, fields, files)=>{
+        // cartModel.findOneAndUpdate({email:fields.email},{},null,(error, result)=>{
+        //     if (error) {
+        //         console.log(error);
+        //         res.send("Error updating the cart.");
+        //     }else{
+        //     }
+        // })
+        cartModel.find({email:fields.email},(error, result)=>{
+            let newCart;
+            let items= result[1]
+            let editIndex = items.findIndex((element)=> element.productId == fields.productId
+            );
+            let edited = parseInt(items[editIndex].count)+1;
+            items[editIndex].count = edited;
+            newCart = [result[0], items];
+            cartModel.updateOne({email:fields.email},{$set: {newCart}}, null, (err, result)=>{
+                if (err) throw err;
+                console.log('Cart updated');
+                res.send('success');
+            })
+        })
+    })
+})
+router.patch('/decrease-item-count',(req,res)=>{
+    let form = new formidable.IncomingForm();
+    form.parse(req, (error, fields, files)=>{
+        cartModel.find({email:fields.email},(error, result)=>{
+            let newCart;
+            let items= result[1]
+            let editIndex = items.findIndex((element)=> element.productId == fields.productId
+            );
+            let edited = parseInt(items[editIndex].count)-1;
+            items[editIndex].count = edited;
+            newCart = [result[0], items];
+            cartModel.updateOne({email:fields.email},{$set: {newCart}}, null, (err, result)=>{
+                if (err) throw err;
+                console.log('Cart updated');
+                res.send('success');
+            })
+        })
+    })
 })
 
 module.exports = router;
