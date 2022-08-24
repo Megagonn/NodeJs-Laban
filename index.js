5c4ecceffa7e252d62eb03525cbdc5198e4cc9f8
Meg <yusuffrasheed.yr@gmail.com>
2022-08-24 09:10:08 +0100

    PATCH method was added to the header.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // }))
 app.use((req,res, next)=>{
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Content-type:application/json");
     next();
